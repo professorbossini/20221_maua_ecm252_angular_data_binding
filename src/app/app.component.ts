5b22575be703444b2785ea9e796d8420984d8656
Rodrigo Bossini <professorbossini@gmail.com>
2022-04-30 13:20:22 -0300

    Encerramos a Listagem 2.4.13 da apostila 04 de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'data-binding';
+  
+  nome: string;
+
+  alterarNome(event){
+    console.log(event.target.value)
+    this.nome = event.target.value;
+
+  }
+
+  adicionar(): void{
+    console.log("Adicionando...")
+  }
+  
+  // lancarDado(): number{
+  //   return Math.floor(Math.random() * 6) + 1;
+  // }
+  // nome = "Ana"
+  // idade = 17
 }
